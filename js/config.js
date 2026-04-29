1489073ec002f98d7c547dd06decdd7b4f98ef7c
IFRS-RG <projetos@riogrande.ifrs.edu.br>
2026-04-28 23:02:50 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@
 // no Google Cloud Console para este projeto.
 // ============================================================
 const SGA_CONFIG = {
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbxKo8XmDxii6-aRAFi4NHhuAqN1hC0BCpou1m5SqLZeVURYQIp-QEZEcGvn2E8Gqgbn/exec',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbyKknsrRoauUSfMzKDZu58qvqBebJAShobrVr6n8HUdpsvqt6t91e8k9pUMIv4GKMpz/exec',
   GOOGLE_CLIENT_ID: '417258387995-5979idejok5shddicb8skm120bv67rbj.apps.googleusercontent.com'
 };
