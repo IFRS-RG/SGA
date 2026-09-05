21492aa65f974c84e99c75caa95515817eef3d4f
thgfonseca <thiago.fonseca@riogrande.ifrs.edu.br>
2026-09-05 08:56:09 -0300

    Portal do aluno: URL /exec do backend no config.js

--- portal/js/config.js
+++ portal/js/config.js
@@ -4,7 +4,7 @@
 // ============================================================
 const PORTAL_CONFIG = {
   // URL /exec do Web App do PORTAL (projeto Apps Script separado).
-  GAS_URL: 'COLE_AQUI_A_URL_EXEC_DO_PORTAL',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbwxHX7ITEP_ZcP_3jNgYehp88JpCTBvSLk6dB6rSJ3xN_I4Md0u56YkrRWYWpjXFrzk8g/exec',
 
   // Mesmo OAuth Client ID do SGA (origem ifrs-rg.github.io já autorizada).
   GOOGLE_CLIENT_ID: '417258387995-5979idejok5shddicb8skm120bv67rbj.apps.googleusercontent.com',
